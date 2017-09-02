f1940c639d1fa947d5085c2553115d2b20d7f84a
Masein Mody <maseinmody@Maseins-MacBook-Air.local>
2017-09-02 08:02:20 -0700

    creating DreamList dataModel without NSManagedObject subclass

---
+++ DreamLister/DreamLister/DreamLister/Image+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  Image+CoreDataClass.swift
+//  DreamLister
+//
+//  Created by Masein Mody on 9/2/17.
+//  Copyright © 2017 Masein Mody. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+@objc(Image)
+public class Image: NSManagedObject {
+
+}
---
+++ DreamLister/DreamLister/DreamLister/Store+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  Store+CoreDataClass.swift
+//  DreamLister
+//
+//  Created by Masein Mody on 9/2/17.
+//  Copyright © 2017 Masein Mody. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+@objc(Store)
+public class Store: NSManagedObject {
+
+}
